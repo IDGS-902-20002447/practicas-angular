d7a7bffbc4aa34667242dd539cec0dca8bb04ab9
IDGS-802-20002447 <vianeygome774@gmail.com>
2023-06-29 20:50:14 -0600

    Commit 29/06/2023 Cambios de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DistanciaComponent } from './practica-Distancia/distancia/distancia.component';
+import { CinepolisComponent } from './practica2-Cinepolis/cinepolis/cinepolis.component';
+import { Practica1ResistenciasComponent } from './practica1-Resistencia/practica1-resistencias/practica1-resistencias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'Distancia',component:DistanciaComponent},
+  {path:'Distancia',component:DistanciaComponent},
+  {path:'Resistencia',component:Practica1ResistenciasComponent},
+  {path:'Cinepolis',component:CinepolisComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Practica1resistenciasModule } from './practica1-Resistencia/practica1resistencias/practica1resistencias.module';
-import { CinepolisComponent } from './practica2-Cinepolis/cinepolis/cinepolis.component';
 import { DistanciaModuleModule } from './practica-Distancia/distancia-module/distancia-module.module';
 import { CinepolisMModule } from './practica2-Cinepolis/cinepolis-m/cinepolis-m.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
         ],
   imports: [
     BrowserModule,
